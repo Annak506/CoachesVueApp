451a233a5503edfe9b2cd58adf3fb06e93c5b5a9
Annak506 <annakleinloog@hotmail.com>
2024-11-18 11:32:54 +0300

    added theHeader of the application

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
+    <div>
+    <the-header></the-header>
     <router-view></router-view>
+    </div>  
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default{
+    components:{
+        TheHeader
+    }
+}
+</script>
 
 <!-- went back in time in terms of commits and now at the video
      Working on the main layout and styling -->
