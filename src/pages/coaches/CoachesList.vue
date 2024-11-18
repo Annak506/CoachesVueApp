b26dc26d05ece70209ac806bea1d04f22c5a86d4
Annak506 <annakleinloog@hotmail.com>
2024-11-18 12:10:11 +0300

    added vueX and dummy coach data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,11 +9,29 @@
                 <router-link to="/register">Register as a coach</router-link> 
 
             </div>
-            <ul> 
-            LIST OF COACHES</ul>
+            <ul v-if="hasCoaches"> 
+            <li v-for="coach in filteredCoaches" :key="coach.id">
+                {{  coach.firstName }}
+            </li>
+        </ul>
+        <h3 v-else>No coaches found.</h3>
         </section>
     </div>
 </template>
+
+<script>
+export default{
+    computed:{
+        filteredCoaches(){
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches(){
+            return this.$store.getters['coaches/hasCoaches'];
+        }
+    }
+}
+</script>
+
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
